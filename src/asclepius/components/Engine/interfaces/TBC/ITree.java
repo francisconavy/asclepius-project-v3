501d29ec59bd83eba8b1995cd0826b242bd9f9f2
SatouYuri <yurimetal4d@gmail.com>
2019-05-31 15:52:04 -0300

    mpompolçmmpçkop]j 4 ´k=[4 283765 2403 16

--- src/asclepius/components/Engine/interfaces/TBC/ITree.java
+++
@@ -1,6 +0,0 @@
-package asclepius.components.Engine.interfaces.TBC;
-import asclepius.components.Engine.interfaces.all.ITreeTestTools;
-import asclepius.components.Engine.interfaces.all.ITreeDataRequest;
-
-public interface ITree extends ITreeDataRequest, ITreeTestTools{ //Adicionar mais interfaces depois...
-}
